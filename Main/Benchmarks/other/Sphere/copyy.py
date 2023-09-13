49d646be07ec4f60b8acd3ddae84ae8c0d8b8e3e
Agnes <a.e.hendrickx@students.uu.nl>
2023-09-13 12:38:52 +0200

    remove files3

--- Main/Benchmarks/other/Sphere/copyy.py
+++
@@ -1,9 +0,0 @@
-#subroutine compute_benchmark_sphere(rs,dr,Brefx,Brefy,Brefz,Magx,Magy,Magz,Xv,theta,phi,x0,y0,z0)
-import numpy as np
-from math import pi as pi
-#pi=math.pi
-
-print (pi)
-d=pi*4
-
-print(d)
