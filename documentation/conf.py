51cd30e1606c956241d3b001cf723caff0240bce
Agnes <a.e.hendrickx@students.uu.nl>
2023-11-09 20:58:09 +0100

    update_09_11_23

--- documentation/conf.py
+++ documentation/conf.py
@@ -3,8 +3,8 @@
 import os
 import sys
 #sys.path.insert(0, pathlib.Path(__file__).parents[2].resolve().as_posix())
-#sys.path.insert(0, os.path.abspath(".."))
-sys.path.insert(0, os.path.abspath("../.."))
+sys.path.insert(0, os.path.abspath(".."))
+#sys.path.insert(0, os.path.abspath("../.."))
 
 sys.path.append(os.path.abspath("./_ext"))
 
